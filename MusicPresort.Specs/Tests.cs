281d16b48309efc58670edb04aa80fabec99cdc7
Nathan Chere <git@nathanchere.com.au>
2014-09-15 18:36:33 +0200

    Fleshing out classes for specs

---
+++ MusicPresort.Specs/Tests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace MusicPresort.Specs
+{
+    class MusicFolderTests
+    {
+        private MusicFolder _folder;
+
+        [Given(@"I have a music folder")]
+        public void GivenIHaveAMusicFolder()
+        {
+            _folder = new MusicFolder();
+        }
+
+        [Given(@"the folder has MP3s with mixed artist names")]
+        public void GivenTheFolderHasMPsWithMixedArtistNames()
+        {
+            _folder.Add(new MusicFile{});
+        }
+
+
+    }
+
+    public class MusicFolder
+    {
+        public void Add(MusicFile musicFile)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class MusicFile
+    {
+        public string ArtistName { get; set; }
+    }
+}
