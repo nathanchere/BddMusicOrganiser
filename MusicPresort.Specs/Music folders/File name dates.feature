171bbc95f08f4e400066cd03458048bbcb7278c1
Nathan Chere <git@nathanchere.com.au>
2014-09-17 19:27:26 +0200

    Import date spec

---
+++ MusicPresort.Specs/Music folders/File name dates.feature
@@ -0,0 +1,16 @@
+﻿Feature: Catalogue pre-import
+	In order to know when an album was added to the catalog
+	As a thingy
+	I want to determine the import date from a music folder
+
+#Scenario: No file date
+	#Given I have a music folder
+	#And the folder name begins with a recognised timestamp format
+	#When I pre-process the folder
+#	Then the result should be 120 on the screen
+
+Scenario: Valid date
+	Given I have a music folder
+	And the folder name is "2010-12-31 (2014) SomeArtist SomeAlbum 320Kbps MP3 [soundcloud]"
+	When I pre-process the folder
+	Then the result should have the date 31-12-2010
---
+++ MusicPresort.Specs/Tests.PreImport.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace MusicPresort.Specs
+{
+    partial class MusicFolderTests
+    {
+        [Given(@"the folder name is ""(.*)""")]
+        public void GivenTheFolderNameIs(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I pre-process the folder")]
+        public void WhenIPre_ProcessTheFolder()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the result should have the date (.*)(.*)")]
+        public void ThenTheResultShouldHaveTheDate(string p0, int p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
