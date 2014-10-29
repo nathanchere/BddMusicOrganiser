b646576b2ebf196766672132f057346ff04650d2
Nathan Chere <git@nathanchere.com.au>
2014-10-29 20:30:00 +0100

    remove crap from preimport tests

--- MusicPresort.Specs/Specs/Music folders/PreImport.Steps.cs
+++ MusicPresort.Specs/Specs/Music folders/PreImport.Steps.cs
@@ -16,21 +16,6 @@ namespace MusicPresort.Specs
             _orchestratorThingy = new OrchestratorThingy();
         }
 
-        #region Helpers
-        private void AddFile(string artistName, string albumTitle, string trackTitle, int? trackNumber)
-        {
-            throw new NotImplementedException();
-            //_folder._files.Add(
-            //    new MusicFile{
-            //        ArtistName = artistName,
-            //        AlbumTitle = albumTitle,
-            //        TrackTitle = trackTitle,
-            //        TrackNumber = trackNumber
-            //    }
-            //);
-        }
-        #endregion
-
         #region Given
         [Given(@"the folder name is in a valid format")]
         public void GivenTheFolderNameIsInAValidFormat()
@@ -55,79 +40,7 @@ namespace MusicPresort.Specs
         public void GivenIHaveAMusicFolder()
         {
             _folder = new MusicFolder();
-        }
-
-        [Given(@"the folder has MP3s with mixed artist names")]
-        public void GivenTheFolderHasMP3sWithMixedArtistNames()
-        {
-            AddFile("Some artist", "Some album", "Track 1", 1);
-            AddFile("Some artist", "Some album", "Track 2", 2);
-            AddFile("Another artist", "Some album", "Track 3", 3);
-        }
-
-        [Given(@"the folder has MP3s with missing track titles")]
-        public void GivenTheFolderHasMP3sWithMissingTrackTitles()
-        {
-            AddFile("Some artist", "Some album", "Track 1", 1);
-            AddFile("Some artist", "Some album", "", 2);
-            AddFile("Some artist", "Some album", "Track 3", 3);
-        }
-
-        [Given(@"the folder has MP3s with an incomplete sequence of track numbers")]
-        public void GivenTheFolderHasMP3sWithAnIncompleteSequenceOfTrackNumbers()
-        {
-            AddFile("Some artist", "Some album", "Track 1", 1);
-            AddFile("Some artist", "Some album", "Track 2", 2);
-            AddFile("Some artist", "Some album", "Track 4", 4);
-        }
-
-
-        [Given(@"the folder has MP3s with missing track numbers")]
-        public void GivenTheFolderHasMP3sWithMissingTrackNumbers()
-        {
-            AddFile("Some artist", "Some album", "Track 1", 1);
-            AddFile("Some artist", "Some album", "Track 2", null);
-            AddFile("Some artist", "Some album", "Track 3", 3);
-        }
-
-        [Given(@"the folder has MP3s with invalid track numbers")]
-        public void GivenTheFolderHasMP3sWitInvalidTrackNumbers()
-        {
-            AddFile("Some artist", "Some album", "Track 1", 1);
-            AddFile("Some artist", "Some album", "Track 2", 0);
-            AddFile("Some artist", "Some album", "Track 3", 3);
-        }
-
-        [Given(@"the folder has MP3s with missing album titles")]
-        public void GivenTheFolderHasMP3sWithMissingAlbumTitles()
-        {
-            AddFile("Some artist", "", "Track 1", 1);
-            AddFile("Some artist", "", "Track 2", 2);
-            AddFile("Some artist", "", "Track 3", 3);
-        }
-
-
-        [Given(@"the folder has MP3s with missing artist names")]
-        public void GivenTheFolderHasMP3sWithMissingArtistNames()
-        {
-            AddFile("", "Some album", "Track 1", 1);
-            AddFile("", "Some album", "Track 2", 2);
-            AddFile("", "Some album", "Track 3", 3);
-        }
-
-        [Given(@"the folder has MP3s with mixed album titles")]
-        public void GivenTheFolderHasMP3sWithMixedAlbumTitles()
-        {
-            AddFile("Some artist", "Some album", "Track 1", 1);
-            AddFile("Some artist", "Different album", "Track 2", 2);
-            AddFile("Some artist", "Some album", "Track 3", 3);
-        }
-
-        [Given(@"the folder has no MP3s")]
-        public void GivenTheFolderHasNoMP3s()
-        {
-            // Nothing to do! :)
-        }
+        }      
         #endregion
 
         #region When        
