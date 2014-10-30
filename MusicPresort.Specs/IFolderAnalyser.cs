9571e9b870f326e9e9243a33e7ceb33f4d1292d9
Nathan Chere <git@nathanchere.com.au>
2014-10-30 09:20:14 +0100

    Remove orchestrator from analysis testing scope

--- MusicPresort.Specs/IFolderAnalyser.cs
+++ MusicPresort.Specs/IFolderAnalyser.cs
@@ -1,10 +1,70 @@
-﻿namespace MusicPresort
+﻿using System;
+using System.Linq;
+using MusicPresort.Specs;
+
+namespace MusicPresort
 {
     public interface IFolderAnalyser
     {
+        AnalysisCache Analyse(MusicFolder folder);
+        bool IsValid(MusicFolder folder);
     }
 
     public class FolderAnalyser : IFolderAnalyser
     {
+        private readonly Func<MusicFile, bool> IsArtistMissing;
+        private readonly Func<MusicFile, bool> IsAlbumMissing;
+        private readonly Func<MusicFile, bool> IsTrackNumberMissing;
+        private readonly Func<MusicFile, bool> IsTrackNumberInvalid;
+        private readonly Func<MusicFolder, bool> IsTrackNumberSequenceIncomplete;
+        private readonly Func<MusicFile, bool> IsTrackNameMissing;
+
+        public AnalysisCache Analyse(MusicFolder folder)
+        {
+            var result = new AnalysisCache();
+            return result;
+        }
+
+        public bool IsValid(MusicFolder folder)
+        {
+            if (!folder.Files.Any()) return false;
+
+            if (folder.Files.Any(IsArtistMissing)) return false;
+
+            if (folder.Files.Any(IsAlbumMissing)) return false;
+
+            if (folder.Files.Any(IsTrackNumberMissing)) return false;
+
+            if (folder.Files.Any(IsTrackNameMissing)) return false;
+
+            if (folder.Files.Any(IsTrackNumberInvalid)) return false;
+
+            if (IsTrackNumberSequenceIncomplete(folder)) return false;
+
+            if (folder.Files.Any(x => x.ArtistName != folder.Files[0].ArtistName)) return false;
+
+            if (folder.Files.Any(x => x.AlbumTitle != folder.Files[0].AlbumTitle)) return false;
+
+            return true;
+        }
+
+        public FolderAnalyser()
+        {
+            IsArtistMissing = f => string.IsNullOrEmpty(f.ArtistName);
+            IsAlbumMissing = f => string.IsNullOrEmpty(f.AlbumTitle);
+            IsTrackNumberMissing = f => !f.TrackNumber.HasValue;
+            IsTrackNumberInvalid = f => !f.TrackNumber.HasValue || f.TrackNumber.Value <= 0;
+            IsTrackNumberSequenceIncomplete = f =>
+            {
+                int i = 1;
+                foreach (var file in f.Files.OrderBy(x => x.TrackNumber))
+                {
+                    if (file.TrackNumber != i) return true;
+                    i++;
+                }
+                return false;
+            };
+            IsTrackNameMissing = f => string.IsNullOrWhiteSpace(f.TrackTitle);
+        }
     }
 }
--- MusicPresort.Specs/OrchestratorThingy.cs
+++ MusicPresort.Specs/OrchestratorThingy.cs
@@ -13,14 +13,6 @@ namespace MusicPresort
         public List<MusicFolder> BadFolders { get; private set; }
         public string RootFolder { get; set; }
 
-        //TODO: probably move to some kind of folderValidator
-        private readonly Func<MusicFile, bool> IsArtistMissing;
-        private readonly Func<MusicFile, bool> IsAlbumMissing;
-        private readonly Func<MusicFile, bool> IsTrackNumberMissing;
-        private readonly Func<MusicFile, bool> IsTrackNumberInvalid;
-        private readonly Func<MusicFolder, bool> IsTrackNumberSequenceIncomplete;
-        private readonly Func<MusicFile, bool> IsTrackNameMissing;
-
         private IFolderAnalyser _analyser;
         private IFolderProcessor _processor;
 
@@ -35,54 +27,15 @@ namespace MusicPresort
             GoodFolders = new List<MusicFolder>();
             BadFolders = new List<MusicFolder>();
             Results = new List<PreprocessResult>();
-
-            IsArtistMissing = f => string.IsNullOrEmpty(f.ArtistName);
-            IsAlbumMissing = f => string.IsNullOrEmpty(f.AlbumTitle);
-            IsTrackNumberMissing = f => !f.TrackNumber.HasValue;
-            IsTrackNumberInvalid = f => !f.TrackNumber.HasValue || f.TrackNumber.Value <= 0;
-            IsTrackNumberSequenceIncomplete = f =>
-            {
-                int i = 1;
-                foreach (var file in f.Files.OrderBy(x => x.TrackNumber))
-                {
-                    if (file.TrackNumber != i) return true;
-                    i++;
-                }
-                return false;
-            };
-            IsTrackNameMissing = f => string.IsNullOrWhiteSpace(f.TrackTitle);
-        }
-
-        public bool IsValid(MusicFolder folder)
-        {
-            if (!folder.Files.Any()) return false;
-
-            if (folder.Files.Any(IsArtistMissing)) return false;
-
-            if (folder.Files.Any(IsAlbumMissing)) return false;
-
-            if (folder.Files.Any(IsTrackNumberMissing)) return false;
-
-            if (folder.Files.Any(IsTrackNameMissing)) return false;
-
-            if (folder.Files.Any(IsTrackNumberInvalid)) return false;
-
-            if (IsTrackNumberSequenceIncomplete(folder)) return false;
-
-            if (folder.Files.Any(x => x.ArtistName != folder.Files[0].ArtistName)) return false;
-
-            if (folder.Files.Any(x => x.AlbumTitle != folder.Files[0].AlbumTitle)) return false;
-
-            return true;
         }
        
         public void ProcessFolder(MusicFolder folder)
         {
             if (folder.Analysis != null) return;
 
-            if(!IsValid(folder)) _processor.Process(folder);
+            if(!_analyser.IsValid(folder)) _processor.Process(folder);
 
-            if (!IsValid(folder))
+            if (!_analyser.IsValid(folder))
                 BadFolders.Add(folder); // TODO: probably change these tests
             else
                 GoodFolders.Add(folder);
